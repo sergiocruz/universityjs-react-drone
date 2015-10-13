0d5dbe3c805932c225d04f70620e2ec1c5b8a1b2
David Khourshid <davidkpiano@gmail.com>
2015-10-13 19:40:15 -0400

    More stuff

--- app/components/steps/step-final.js
+++ app/components/steps/step-final.js
@@ -19,30 +19,38 @@ export default ({ status, takeoff, land, emergency, move, turn }) => {
 
       <ul className="d-arrows left">
         <li className="d-arrow">
+          <i className="fa fa-arrow-up"></i>
           <a href="" onClick={move.bind(null, 'forward')}>Move forward</a>
         </li>
         <li className="d-arrow">
+          <i className="fa fa-arrow-right"></i>
           <a href="" onClick={move.bind(null, 'right')}>Move right</a>
         </li>
         <li className="d-arrow">
+          <i className="fa fa-arrow-left"></i>
           <a href="" onClick={move.bind(null, 'left')}>Move left</a>
         </li>
         <li className="d-arrow">
+          <i className="fa fa-arrow-down"></i>
           <a href="" onClick={move.bind(null, 'backward')}>Move backward</a>
         </li>
       </ul>
 
       <ul className="d-arrows right">
         <li className="d-arrow">
+          <i className="fa fa-arrow-up"></i>
           <a href="" onClick={turn.bind(null, 'up')}>Rise drone</a>
         </li>
         <li className="d-arrow">
+          <i className="fa fa-arrow-right"></i>
           <a href="" onClick={turn.bind(null, 'right')}>Turn right</a>
         </li>
         <li className="d-arrow">
+          <i className="fa fa-arrow-left"></i>
           <a href="" onClick={turn.bind(null, 'down')}>Lower drone</a>
         </li>
         <li className="d-arrow">
+          <i className="fa fa-arrow-down"></i>
           <a href="" onClick={turn.bind(null, 'left')}>Turn left</a>
         </li>
       </ul>
